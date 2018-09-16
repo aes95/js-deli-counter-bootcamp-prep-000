257649c0bfaa57bbfd90b77a0708adb01e93c818
Aviad Sapir <aviadsapir@gmail.com>
2018-09-16 17:14:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function takeANumber(line, name){
-  var num = line + 1
-  return `Welcome, ${name}. You are number ${num} in line.`
+  var num = line + 1;
+  return `Welcome, ${name}. You are number ${num} in line.`;
 }
